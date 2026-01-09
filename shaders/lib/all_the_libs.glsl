e6350ae35cb47335850fce341ffa62e51c823a1c
Epxlsol <77103326+Epxlsol@users.noreply.github.com>
2026-01-09 16:34:04 +0000

    Update all_the_libs.glsl

--- shaders/lib/all_the_libs.glsl
+++ shaders/lib/all_the_libs.glsl
@@ -1,5 +1,13 @@
+// Fix for the "Undeclared" error
 #if !defined f_BIOME_SKY_CONTRIBUTION
     #define f_BIOME_SKY_CONTRIBUTION 1.0
+#endif
+
+// Fix for the "Redeclared" error
+#ifndef COLORTEX_SAMPLERS
+    #define COLORTEX_SAMPLERS
+    uniform sampler2D colortex0;
+    // Add others if needed: uniform sampler2D colortex1;
 #endif
 #extension GL_EXT_gpu_shader4 : enable
 
